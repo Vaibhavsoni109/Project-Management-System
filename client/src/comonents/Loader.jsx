8519d5d837b4825969ad930a1578175c0728c2d2
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-05-10 17:38:06 +0530

    some files added

---
+++ client/src/comonents/Loader.jsx
@@ -0,0 +1,15 @@
+
+const Loading = () => {
+    return (
+      <div className='dots-container'>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+        <div className='dot'></div>
+      </div>
+    );
+  };
+  
+  export default Loading;
+  
--- client/src/pages/Tasks.jsx
+++ client/src/pages/Tasks.jsx
@@ -1,11 +1,81 @@
-import React from 'react'
+import React, { useState } from "react";
+import { FaList } from "react-icons/fa";
+import { MdGridView } from "react-icons/md";
+import { useParams } from "react-router-dom";
+import Loading from "../comonents/Loader";
+import Title from "../comonents/Title";
+import Button from "../comonents/Button";
+import { IoMdAdd } from "react-icons/io";
+// import Tabs from "../components/Tabs";
+// import TaskTitle from "../comonents/TaskTitle";
+// import BoardView from "../comonents/BoardView";
+import { tasks } from "../assets/data";
+// import Table from "../components/task/Table";
+// import AddTask from "../components/task/AddTask";
+
+const TABS = [
+  { title: "Board View", icon: <MdGridView /> },
+  { title: "List View", icon: <FaList /> },
+];
+
+const TASK_TYPE = {
+  todo: "bg-blue-600",
+  "in progress": "bg-yellow-600",
+  completed: "bg-green-600",
+};
 
 const Tasks = () => {
-  return (
-    <div>
-      Tasks
-    </div>
-  )
-}
+  const params = useParams();
 
-export default Tasks
+  const [selected, setSelected] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const status = params?.status || "";
+
+  return loading ? (
+    <div className='py-10'>
+      <Loading />
+    </div>
+  ) : (
+    <div className='w-full'>
+      <div className='flex items-center justify-between mb-4'>
+        <Title title={status ? `${status} Tasks` : "Tasks"} />
+
+        {!status && (
+          <Button
+            onClick={() => setOpen(true)}
+            label='Create Task'
+            icon={<IoMdAdd className='text-lg' />}
+            className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5'
+          />
+        )}
+      </div>
+
+      {/* <Tabs tabs={TABS} setSelected={setSelected}>
+        {!status && (
+          <div className='w-full flex justify-between gap-4 md:gap-x-12 py-4'>
+            <TaskTitle label='To Do' className={TASK_TYPE.todo} />
+            <TaskTitle
+              label='In Progress'
+              className={TASK_TYPE["in progress"]}
+            />
+            <TaskTitle label='completed' className={TASK_TYPE.completed} />
+          </div>
+        )}
+
+        {selected !== 1 ? (
+          <BoardView tasks={tasks} />
+        ) : (
+          <div className='w-full'>
+            <Table tasks={tasks} />
+          </div>
+        )}
+      </Tabs> */}
+
+      {/* <AddTask open={open} setOpen={setOpen} /> */}
+    </div>
+  );
+};
+
+export default Tasks;
