af8dfe7e694407d27afc1d65b69e821e53bca7e8
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-05-17 18:02:10 +0530

    commit

---
+++ server/models/task.js
@@ -0,0 +1,5 @@
+import { Schema } from "mongoose";
+
+const taskShema =new Schema({
+    
+})
--- server/models/user.js
+++ server/models/user.js
@@ -20,5 +20,8 @@ userSchema.pre("save",async function(next){
     const salt=await bcrypt.genSalt(10)
     this.password=await bcrypt.hash(this.password,salt);
 })
+userSchema.methods.matchPassword=async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password);
+}
 const User=mongoose.model("User",userSchema);
 module.exports=User;
