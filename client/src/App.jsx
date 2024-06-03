4aea416acc0f5188aaa9e30ae4a182d95a007c51
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-06-03 11:41:58 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -101,7 +101,7 @@ function App() {
   return <main className='w-full min-h-screen bg-[#f3f4f6]'>
     <Routes>   
       <Route element={<Layout />}>
-        <Route path='/' element={<Navigate to='/dashboard' />} />
+        <Route path='/' element={<Navigate to='/log-in' />} />
         <Route path='/dashboard' element={<Dashboard />} />
         <Route path='/tasks' element={<Tasks />} />
         <Route path='/completed/:status' element={<Tasks />} />
