6bc62b01448fb96b9a0e5e3d5811a39a12c3fd54
Vaibhav Kant Soni <94217024+Vaibhavsoni109@users.noreply.github.com>
2024-06-03 11:28:03 +0530

    commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://project-management-system-16b4.onrender.com",
         changeOrigin: true,
       },
     },
